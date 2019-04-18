24b33ee6a76511ce4978934284cea46c2061c8f9
Vincent Mustillo <vjmustillo@gmail.com>
2019-04-17 23:30:27 -0400

    Reformatted Blockchainstruct to include Badger DB. AddBlock now adds a block to the chain using the last hash in the DB and updates the struct. InitBlockChain now creates the chain or gets the existing chain from the database

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -1,18 +1,86 @@
 package blockchain
 
-// BlockChain is a structure that will hold all blocks in an array
+import (
+	"fmt"
+
+	"github.com/dgraph-io/badger"
+)
+
+const (
+	dbPath = "./tmp/blocks"
+)
+
+// BlockChain is a structure that will hold a pointer to a database contatining all blocks, and the hash of the last block
 type BlockChain struct {
-	Blocks []*Block
+	LastHash []byte
+	Database *badger.DB
 }
 
 // AddBlock takes a string of data and creates a block and adds it to the blockchain
+// Gets the last hash from the chain and creates a new block using the data and last hash
+// Adds the new block to the DB, sets the last hash in the DB, and stores the last hash in the chain struct
 func (chain *BlockChain) AddBlock(data string) {
-	prevBlock := chain.Blocks[len(chain.Blocks)-1]
-	new := CreateBlock(data, prevBlock.Hash)
-	chain.Blocks = append(chain.Blocks, new)
+	var lastHash []byte
+
+	err := chain.Database.View(func(txn *badger.Txn) error {
+		item, err := txn.Get([]byte("lh"))
+		Handle(err)
+		lastHash, err = item.ValueCopy(nil)
+
+		return err
+	})
+	Handle(err)
+
+	newBlock := CreateBlock(data, lastHash)
+	err = chain.Database.Update(func(txn *badger.Txn) error {
+		err := txn.Set(newBlock.Hash, newBlock.Serialize())
+		Handle(err)
+		err = txn.Set([]byte("lh"), newBlock.Hash)
+
+		chain.LastHash = newBlock.Hash
+
+		return err
+	})
+	Handle(err)
 }
 
-//InitBlockChain creates the blockchain with the Genesis block inside
+// InitBlockChain creates the blockchain if there is no existing chain
+// If there is no chain, it creates the chain with the Genesis block iside
+// the function opens the database, creates the Genesis block, stores the block and its hash in the db, and sets its hash to the last hash in the DB
+// If there is already an existing chain, it returns a blockchain with the last hash and database
 func InitBlockChain() *BlockChain {
-	return &BlockChain{[]*Block{Genesis()}}
+	var lastHash []byte
+
+	opts := badger.DefaultOptions
+	opts.Dir = dbPath
+	opts.ValueDir = dbPath
+
+	db, err := badger.Open(opts)
+	Handle(err)
+
+	err = db.Update(func(txn *badger.Txn) error {
+		if _, err := txn.Get([]byte("lh")); err == badger.ErrKeyNotFound {
+			fmt.Println("No existing blockchain found")
+			genesis := Genesis()
+			fmt.Println("Genesis proved")
+			err = txn.Set(genesis.Hash, genesis.Serialize())
+			Handle(err)
+			err = txn.Set([]byte("lh"), genesis.Hash)
+
+			lastHash = genesis.Hash
+
+			return err
+		} else {
+			item, err := txn.Get([]byte("lh"))
+			Handle(err)
+			lastHash, err = item.ValueCopy(nil)
+
+			return err
+		}
+	})
+
+	Handle(err)
+
+	blockchain := BlockChain{lastHash, db}
+	return &blockchain
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module github.com/vmustillo/golang-blockchain
 
 go 1.12
+
+require (
+	github.com/AndreasBriese/bbloom v0.0.0-20190306092124-e2d15f34fcf9 // indirect
+	github.com/dgraph-io/badger v1.5.4
+	github.com/dgryski/go-farm v0.0.0-20190323231341-8198c7b169ec // indirect
+	github.com/golang/protobuf v1.3.1 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
+	github.com/stretchr/testify v1.3.0 // indirect
+	golang.org/x/net v0.0.0-20190415214537-1da14a5a36f2 // indirect
+)
